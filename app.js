f4e6457e7bea8e680c61036ff087fecffe164b08
rashid <abdulrashid.hayato@edu.sait.ca>
2023-02-15 19:53:15 -0700

    last commit

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const dayjs = require('dayjs');
-console.log()
+
+// This indicates the difference between two date-time in the specified unit.
+
+const date1 = dayjs('2001-10-03')
+const date2 = dayjs('2019-01-25')
+
+
+const date3 =("2019-14-05");
+console.log( date3);
+const date5 = '2023-10-03';
+console.log(date5);
+
